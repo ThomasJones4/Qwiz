58fbd1477dcd17f956e691e869fefa4a4a86bb2d
Thomas Jones <github@zm7.uk>
2020-05-23 09:50:55 +0100

    seeders updated

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Quiz;
+use App\Question;
+
 
 class UsersTableSeeder extends Seeder
 {
@@ -17,8 +21,25 @@ class UsersTableSeeder extends Seeder
             'email' => 'admin@argon.com',
             'email_verified_at' => now(),
             'password' => Hash::make('secret'),
+            'api_token' => Str::random(60),
             'created_at' => now(),
             'updated_at' => now()
         ]);
+
+
+        factory(User::class, 10)->create()->each(function ($user) {
+          $user->my_quizzes()->save(factory(Quiz::class)->make());
+          $user->my_quizzes()->each(function ($quiz) {
+            for ($i = 0; $i < 20; $i++) {
+              $quiz->questions()->save(factory(Question::Class)->make(['order' => $i]));
+            }
+          });
+        });
+
+        factory(User::class, 300)->create()->each(function ($user) {
+          // choose a quiz and do questions
+        });
+
+
     }
 }
