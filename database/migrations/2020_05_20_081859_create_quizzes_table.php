41b6adc7ee4624d8bc2c50728ccf47503fc6a4de
Thomas Jones <github@zm7.uk>
2020-05-23 09:50:37 +0100

    Migrations updated

--- database/migrations/2020_05_20_081859_create_quizzes_table.php
+++ database/migrations/2020_05_20_081859_create_quizzes_table.php
@@ -18,6 +18,7 @@ class CreateQuizzesTable extends Migration
             $table->unsignedBigInteger('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users');
             $table->datetime('scheduled_start')->nullable();
+            $table->string('invite_code');
             $table->string('name');
             $table->timestamps();
         });
---
+++ database/migrations/2020_05_21_091150_create_quiz_user_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateQuizUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quiz_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('quiz_id')->unsigned()->index();
+            $table->foreign('quiz_id')->references('id')->on('quizzes')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['quiz_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quiz_user');
+    }
+}
