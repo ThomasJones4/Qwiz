a3870d5670097e31d455db57031fa6a0f9c25b9c
Thomas Jones <github@zm7.uk>
2020-05-28 11:39:05 +0100

    db test added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,13 @@
                 </div>
             </div>
         </div>
+
+        @if(DB::connection()->getDatabaseName())
+
+     conncted sucessfully to database {{DB::connection()->getDatabaseName()}}
+
+     @endif
+
         <div class="separator separator-bottom separator-skew zindex-100">
             <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
